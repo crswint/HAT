eb4703a09ed13d45ec3780c388e87f7065b2240c
crswin5726 <crswin5626@ung.edu>
2015-04-01 11:30:12 -0400

    Set up API
    Took steps to set up API. Added serializers, api_urls, views, to relate to appropriate models (Stops, Routes). Added rest framework settings and project urls in order to map to API urls. Had to update from 3.0 djangorestframework to 3.1, as well as update djangorestframework-gis 0.8 in order to fix errors with importing serializers from 3.0.

--- HAT/urls.py
+++ HAT/urls.py
@@ -2,11 +2,18 @@ from django.conf.urls import patterns, include, url
 from apps.maps import views
 from django.contrib import admin
 
+
+api_patterns = patterns('',
+    url(r'^', include('apps.maps.api_urls'), name='maps_api')
+)
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gisc2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^maps/', include('apps.maps.urls', namespace='maps'))
+    url(r'^maps/', include('apps.maps.urls', namespace='maps')),
+    url(r'^api/v1/', include(api_patterns, namespace='api')),
 )
--- apps/maps/models.py
+++ apps/maps/models.py
@@ -12,16 +12,22 @@ class Stop(models.Model):
     name = models.CharField(max_length=40)
     amenities = models.CharField(max_length=40)
 
+    def __str__(self):
+        return "{}".format(self.name)
+
 
 class Route(models.Model):
     """This model will hold public transit bus routes information"""
-    geom = models.LineStringField(srid=4326)
+    geom = models.MultiLineStringField(srid=4326)
     route_num = models.CharField(max_length=40)
     stops = models.ManyToManyField(Stop)
 
+    def __str__(self):
+        return "{}".format(self.route_num)
+
 
 # class Images(models.Model):
-#     """Pics model."""
+#     """This model will hold image files of Stops."""
 #     name = models.CharField(max_length=100)
 #     place = models.ForeignKey(Stop)
 #     photo = models.ImageField(storage=fs)
